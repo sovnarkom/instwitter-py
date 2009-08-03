19194e4d26f2cceae171635928d300fb847e22ce
sovnarkom <sovnarkom@somebugs.com>
2009-08-04 01:16:56 +0400

    + Added script for distutils

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='instwitter',
+      version='0.9.0',
+      description='Twitter API Library',
+      author='Alecander Chichenin',
+      author_email='sovnarkom@somebugs.com',
+      url='http://github.com/sovnarkom/instwitter-py',
+      packages=['instwitter', 'oauth']
+      )
